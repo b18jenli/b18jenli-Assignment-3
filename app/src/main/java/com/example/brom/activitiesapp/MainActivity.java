ffb8d50f9689fec3f90320cd8f370f8a4ff15637
b18jenli <b18jenli@student.his.se>
2019-04-18 13:49:29 +0200

    Listview - skapad

--- app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
+++ app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
@@ -2,17 +2,53 @@ package com.example.brom.activitiesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
-    private String[] mountainLocations = {"Alps","Alps","Alaska"};
-    private int[] mountainHeights ={4478,4808,6190};
+
+    private String[] mountainNames = {"Matterhorn", "Mont Blanc", "Denali"};
+    private String[] mountainLocations = {"Alps", "Alps", "Alaska"};
+    private int[] mountainHeights = {4478, 4808, 6190};
+
+
+    Mountain m = new Mountain("Matterhorn", "Alps", 4478);
+    Mountain m2 = new Mountain("Mont Blanc", "Alps", 4808);
+    Mountain m3 = new Mountain("Denali", "Alaska", 6190);
+
     // Create ArrayLists from the raw data above and use these lists when populating your ListView.
+    ArrayList<String> berg = new ArrayList<String>(Arrays.asList(mountainNames));
+
+    ArrayList<Mountain> berg2=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        berg2.add(m);
+        berg2.add(m2);
+        berg2.add(m3);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item_textview, R.id.list_item_textview, berg);
+        ListView lista= findViewById(R.id.listview);
+        lista.setAdapter(adapter);
+
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), berg2.get(position).info(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
+
 
         // 1. Create a ListView as in previous assignment
         // 2. Create a new activity named "MountainDetailsActivity
@@ -33,5 +69,3 @@ public class MainActivity extends AppCompatActivity {
         // 8. From the MountainDetailsActivity you should have an option to "go back" using an
         //    left arro button. This is done by letting the MainActivity be the parent activity to
         //    MountainDetailsActivity.
-    }
-}
---
+++ app/src/main/java/com/example/brom/activitiesapp/Mountain.java
@@ -0,0 +1,22 @@
+package com.example.brom.activitiesapp;
+
+public class Mountain {
+    private String name;
+    private String location;
+    private int height;
+
+    public Mountain(String n, String l, int h){
+        name = n;
+        location = l;
+        height = h;
+    }
+    public String info(){
+        String tmp;
+        tmp = location + " " + height;
+        return tmp;
+    }
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
